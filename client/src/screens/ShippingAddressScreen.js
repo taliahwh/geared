90c4450f8c35b81e6ff768e5f2bf727a354f4576
Taliah Wharton <taliahwh23@yahoo.com>
2022-08-23 18:40:33 -0400

    offer, checkout, shipping address screen ui

---
+++ client/src/screens/ShippingAddressScreen.js
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import { View, TextInput, Text, TouchableOpacity } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import RNPickerSelect from 'react-native-picker-select';
+
+import { states } from '../utils/states';
+
+// Styles
+import {
+  styles,
+  pickerSelectStyles,
+} from '../styles/ShippingAddressScreenStyles';
+const ShippingAddressScreen = () => {
+  const [address, setAddress] = useState('');
+  const [addressLineTwo, setAddressLineTwo] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [zipCode, setZipCode] = useState(null);
+  const [sport, setSport] = useState(null);
+
+  const submitAddress = () => {
+    const newAddress = {
+      address,
+      addressLineTwo,
+      city,
+      state,
+      zipCode,
+      sport,
+    };
+
+    console.log(newAddress);
+  };
+
+  const placeholder = {
+    label: 'Select',
+    value: null,
+    color: '#9EA0A4',
+  };
+
+  return (
+    <KeyboardAwareScrollView style={styles.screen}>
+      <View style={styles.container}>
+        <Text style={styles.inputTitle}>Address line 1</Text>
+        <TextInput
+          style={styles.textInput}
+          textContentType="fullStreetAddress"
+          value={address}
+          onChange={setAddress}
+        />
+
+        <Text style={styles.inputTitle}>Address line 2</Text>
+        <TextInput
+          placeholder="Optional"
+          style={styles.textInput}
+          value={addressLineTwo}
+          onChange={setAddressLineTwo}
+        />
+
+        <Text style={styles.inputTitle}>City</Text>
+        <TextInput
+          style={styles.textInput}
+          textContentType="addressCity"
+          value={city}
+          onChangeText={setCity}
+        />
+
+        <Text style={styles.inputTitle}>State</Text>
+        <View style={pickerSelectStyles.pickerContainer}>
+          <RNPickerSelect
+            style={pickerSelectStyles}
+            onValueChange={(value) => setSport(value)}
+            items={states}
+            placeholder={placeholder}
+          />
+        </View>
+
+        <Text style={styles.inputTitle}>Zip or postal code</Text>
+        <TextInput
+          style={styles.textInput}
+          textContentType="postalCode"
+          value={zipCode}
+          onChangeText={setZipCode}
+        />
+
+        <View style={styles.countrySection}>
+          <Text style={styles.inputTitle}>Country</Text>
+          <Text style={styles.countryText}>United States</Text>
+        </View>
+
+        <TouchableOpacity onPress={submitAddress} activeOpacity={0.8}>
+          <View style={styles.btnContainer}>
+            <Text style={styles.continueBtn}>Continue</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAwareScrollView>
+  );
+};
+
+export default ShippingAddressScreen;
