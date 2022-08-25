5ba66201b659f6dba4eefeb82919cdd37824e720
Taliah Wharton <taliahwh23@yahoo.com>
2022-08-25 15:34:37 -0400

    added modal for selecting address

---
+++ client/src/components/SelectAddressModal.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// Styles
+import styles from '../styles/SelectAddressModalStyles';
+
+const SelectAddressModal = () => {
+  return (
+    <View style={styles.modalContainer}>
+      <Text>SelectAddressModal</Text>
+    </View>
+  );
+};
+
+export default SelectAddressModal;
