ff2e20b636016664750377d9bef1fac040496e75
Taliah Wharton <taliahwh23@yahoo.com>
2022-08-29 15:07:55 -0400

    checkout item workflow

--- client/src/components/SelectAddressModal.js
+++ client/src/components/SelectAddressModal.js
@@ -1,13 +1,83 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  Pressable,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 // Styles
 import styles from '../styles/SelectAddressModalStyles';
+import theme from '../styles/styles.theme';
 
-const SelectAddressModal = () => {
+const AddressContainer = ({
+  name,
+  streetAddressOne,
+  streetAddressTwo,
+  city,
+  state,
+  zipCode,
+  country,
+}) => {
   return (
-    <View style={styles.modalContainer}>
-      <Text>SelectAddressModal</Text>
+    <View style={styles.addressContainer}>
+      <Text style={styles.address}>
+        {name}, {streetAddressOne}
+        {streetAddressTwo ? `, ${streetAddressTwo}` : ''}, {city}, {state},{' '}
+        {zipCode}, {country}
+      </Text>
+
+      <View style={styles.chevronContainer}>
+        <Ionicons
+          name="chevron-forward-outline"
+          size={20}
+          color={theme.BORDER_COLOR}
+        />
+      </View>
+    </View>
+  );
+};
+
+const SelectAddressModal = ({ hideModal }) => {
+  return (
+    <View style={styles.screenView}>
+      <View style={styles.modalView}>
+        <View style={styles.headerContainer}>
+          {/* <View style={styles.swipeBar} /> */}
+
+          <View style={styles.header}>
+            <Pressable onPress={hideModal}>
+              <Ionicons name="chevron-back-outline" size={28} color="black" />
+            </Pressable>
+
+            <Text style={styles.headerTitle}>Select address</Text>
+            <Text style={styles.editText}>Edit</Text>
+          </View>
+        </View>
+
+        <View style={styles.addressAndBtnContainer}>
+          <AddressContainer
+            name={'Taliah Wharton'}
+            streetAddressOne={'7838 Provident Street'}
+            streetAddressTwo={null}
+            city={'Philadelphia'}
+            state={'Pennsylvania'}
+            zipCode={'19150'}
+            country={'United States'}
+          />
+
+          <TouchableOpacity
+            onPress={() => console.log('Add new address')}
+            activeOpacity={0.8}
+          >
+            <View style={styles.btnContainer}>
+              <Text style={styles.addAddressBtn}>Add new address</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
