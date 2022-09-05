2fc77978e584319b21d62af68516de97db691a8e
Taliah Wharton <taliahwh23@yahoo.com>
2022-09-05 11:31:49 -0400

    dark mode integration

---
+++ client/src/screens/SoldItemsScreen.js
@@ -0,0 +1,109 @@
+import React, { useRef, useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
+import { useFocusEffect, useScrollToTop } from '@react-navigation/native';
+import { useDispatch, useSelector } from 'react-redux';
+
+// Components
+import SoldItemCard from '../components/SoldItemCard';
+import AlertMessage from '../components/AlertMessage';
+
+// Actions
+
+// Styles
+import theme from '../styles/styles.theme';
+
+const SoldItemsScreen = ({ route }) => {
+  // Hooks
+
+  const scrollRef = useRef(null);
+  useScrollToTop(scrollRef);
+  const dispatch = useDispatch();
+
+  // const renderItem = ({ item }) => {
+  //   // console.log(item);
+  //   return (
+  //     <FollowersCard
+  //       name={item.name}
+  //       username={item.username}
+  //       profileImage={item.profileImage}
+  //       followerId={item._id}
+  //     />
+  //   );
+  // };
+
+  // useFocusEffect(
+  //   React.useCallback(() => {
+  //     dispatch(viewFollowers(userId));
+  //   }, [dispatch])
+  // );
+
+  return (
+    // <>
+    //   {errorFollowers && (
+    //     <View style={styles.centered}>
+    //       <AlertMessage>{errorFollowers}</AlertMessage>
+    //     </View>
+    //   )}
+
+    //   {loadingFollowers && (
+    //     <View style={styles.centered}>
+    //       <ActivityIndicator />
+    //     </View>
+    //   )}
+
+    //   {followers && followers.length > 0 && (
+    //     <View style={styles.container}>
+    //       <FlatList
+    //         ref={scrollRef}
+    //         data={followers.reverse()}
+    //         renderItem={renderItem}
+    //         keyExtractor={(item) => item._id}
+    //       />
+    //     </View>
+    //   )}
+
+    //   {followers && followers.length === 0 && (
+    //     <View style={styles.centered}>
+    //       <Text style={styles.text}>No followers.</Text>
+    //     </View>
+    //   )}
+    // </>
+    <View style={styles.container}>
+      <SoldItemCard
+        username={'buy_thebook'}
+        profileImage={
+          'https://fansided.com/wp-content/uploads/getty-images/2018/11/156442268.jpeg'
+        }
+        orderStatus={'Sale complete'}
+        purchasedDate={'SOLD 1 WEEK AGO'}
+        itemImage={
+          'https://cconnect.s3.amazonaws.com/wp-content/uploads/2019/01/Top-Luka-Doncic-Rookie-Cards-thumb-300.jpg'
+        }
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.FEED_BACKGROUND,
+  },
+  centered: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontStyle: 'italic',
+  },
+});
+
+export default SoldItemsScreen;
