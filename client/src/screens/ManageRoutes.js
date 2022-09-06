9322f288a126f9486f1d79799b2a938991875b4a
Taliah Wharton <taliahwh23@yahoo.com>
2022-09-06 15:47:53 -0400

    styling update

--- client/src/screens/ManageRoutes.js
+++ client/src/screens/ManageRoutes.js
@@ -210,7 +210,7 @@ const styles = StyleSheet.create({
   },
   listAnItemBtn: {
     textAlign: 'center',
-    backgroundColor: '#404040',
+    backgroundColor: theme.PRIMARY_COLOR,
     color: theme.LIGHT_GRAY,
     fontWeight: '600',
     fontSize: 15,
