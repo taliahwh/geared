a9614b77ce889e0188d03d22625801e9732ecfaa
Taliah Wharton <taliahwh23@yahoo.com>
2022-09-08 13:47:29 -0400

    review workflow

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -86,6 +86,10 @@ const userSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+    totalReviewStars: {
+      type: Number,
+      default: 0,
+    },
     reviewAvg: {
       type: Number,
       default: 0,
