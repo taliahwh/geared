ec6783c30c523ff260dbd8c044d1c1abae4e9ed4
Taliah Wharton <taliahwh23@yahoo.com>
2022-09-12 13:04:56 -0400

    search posts workflow

--- client/src/screens/SearchScreen.js
+++ client/src/screens/SearchScreen.js
@@ -8,30 +8,53 @@ import {
   Image,
   ScrollView,
   Dimensions,
+  Modal,
+  Pressable,
+  ActivityIndicator,
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { useDispatch, useSelector } from 'react-redux';
 import { FlatGrid, SectionGrid } from 'react-native-super-grid';
+
+// Components
+import FilterSearchModal from '../components/FilterSearchModal';
 
 // Styles
 import theme from '../styles/styles.theme';
 import styles from '../styles/SearchScreenStyles';
 
+// Actions
+import { searchPosts } from '../actions/postActions';
+
 const windowWidth = Dimensions.get('window').width / 3;
 
-const SearchResult = ({ imageUrl }) => {
+const SearchResult = ({ postImage }) => {
+  console.log('test');
   return (
     <Image
       style={styles.itemContainer}
       resizeMode="cover"
       source={{
-        uri: 'https://res.cloudinary.com/geared/image/upload/v1660571880/post-images/image_876545.jpg',
+        uri: postImage,
       }}
     />
   );
 };
 
 const SearchScreen = () => {
+  // Hooks
+  const dispatch = useDispatch();
+
   const [query, setQuery] = useState('');
+  const [optionsModalVisible, setOptionsModalVisible] = useState(false);
+
+  // Redux state
+  const {
+    loading: loadingSearchResults,
+    searchResults,
+    numResults,
+    error: errorSearchResults,
+  } = useSelector((state) => state.searchPosts);
 
   const [items, setItems] = React.useState([
     { name: 'TURQUOISE', code: '#1abc9c' },
@@ -42,74 +65,101 @@ const SearchScreen = () => {
     { name: 'GREEN SEA', code: '#16a085' },
     { name: 'NEPHRITIS', code: '#27ae60' },
     { name: 'BELIZE HOLE', code: '#2980b9' },
-    { name: 'WISTERIA', code: '#8e44ad' },
-    { name: 'MIDNIGHT BLUE', code: '#2c3e50' },
-    { name: 'SUN FLOWER', code: '#f1c40f' },
-    { name: 'CARROT', code: '#e67e22' },
-    { name: 'ALIZARIN', code: '#e74c3c' },
-    { name: 'CLOUDS', code: '#ecf0f1' },
-    { name: 'CONCRETE', code: '#95a5a6' },
-    { name: 'ORANGE', code: '#f39c12' },
-    { name: 'PUMPKIN', code: '#d35400' },
-    { name: 'POMEGRANATE', code: '#c0392b' },
-    { name: 'SILVER', code: '#bdc3c7' },
-    { name: 'ASBESTOS', code: '#7f8c8d' },
   ]);
+
+  const searchQuery = () => {
+    dispatch(searchPosts(query));
+  };
 
   return (
     <View style={styles.container}>
       {/* Search Input Header */}
       <View style={styles.headerContainer}>
         <View style={styles.textInputContainer}>
-          <Ionicons
-            name="search"
-            size={19}
-            color={theme.DARK_MODE_BORDER}
-            style={styles.searchIcon}
-          />
-          <TextInput
-            value={query}
-            onChangeText={setQuery}
-            placeholder={'Search for cards'}
-            placeholderTextColor={theme.DARK_MODE_BORDER}
-            style={styles.textInput}
-          />
+          <View style={styles.inputAndIcon}>
+            <Ionicons
+              name="search"
+              size={19}
+              color={theme.DARK_MODE_BORDER}
+              style={styles.searchIcon}
+            />
+            <TextInput
+              value={query}
+              onChangeText={setQuery}
+              placeholder={'Search for cards'}
+              placeholderTextColor={theme.DARK_MODE_BORDER}
+              style={styles.textInput}
+              maxLength={50}
+            />
+          </View>
+          {!query.length ? (
+            <Ionicons
+              name="checkmark"
+              size={25}
+              color={'transparent'}
+              style={styles.confirmIcon}
+            />
+          ) : (
+            <Pressable onPress={searchQuery}>
+              <Ionicons
+                name="checkmark"
+                size={25}
+                color={theme.PRIMARY_COLOR}
+                style={styles.confirmIcon}
+              />
+            </Pressable>
+          )}
         </View>
-        <Ionicons
-          name="checkmark"
-          size={25}
-          color={theme.LIGHT_GRAY}
-          style={styles.searchIcon}
-        />
+
+        {/* Filter Component */}
+        {!query.length ? (
+          <Ionicons name="md-options" size={24} color={theme.MEDIUM_GRAY} />
+        ) : (
+          <Pressable onPress={() => setOptionsModalVisible(true)}>
+            <Ionicons name="md-options" size={24} color={theme.LIGHT_GRAY} />
+          </Pressable>
+        )}
       </View>
 
-      {/* <FlatGrid
-        itemDimension={130}
-        data={items}
-        style={styles.gridView}
-        // staticDimension={300}
-        // fixed
-        spacing={10}
-        renderItem={({ item }) => (
-          <View style={[styles.itemContainer, { backgroundColor: 'red' }]} />
-          // <SearchResult />
-        )}
-      /> */}
+      {/* Search Results Grid */}
 
-      <SectionGrid
-        itemDimension={windowWidth}
-        // staticDimension={windowWidth}
-        // fixed
-        spacing={7}
-        sections={[
-          {
-            title: 'Title1',
-            data: items.slice(0, 7),
-          },
-        ]}
-        style={styles.gridView}
-        renderItem={({ item, section, index }) => <SearchResult />}
-      />
+      {loadingSearchResults && (
+        <View style={{ marginTop: 10 }}>
+          <ActivityIndicator />
+        </View>
+      )}
+
+      {numResults > 0 && searchResults && (
+        <SectionGrid
+          itemDimension={windowWidth}
+          // staticDimension={windowWidth}
+          // fixed
+          spacing={7}
+          sections={[
+            {
+              title: 'Search Results',
+              data: searchResults,
+            },
+          ]}
+          style={styles.gridView}
+          renderItem={({ item }) => (
+            <SearchResult postImage={item.images[0].imgUrl} />
+          )}
+        />
+      )}
+
+      {/* Modal */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={optionsModalVisible}
+        onRequestClose={() => {
+          console.log('Modal has been closed.');
+          setOptionsModalVisible(!optionsModalVisible);
+        }}
+      >
+        <FilterSearchModal hideModal={() => setOptionsModalVisible(false)} />
+      </Modal>
     </View>
   );
 };
