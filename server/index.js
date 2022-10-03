7c614ed2967f76f819fae2e2ec0fbed08aa76288
Taliah Wharton <taliahwh23@yahoo.com>
2022-10-03 16:49:06 -0400

    paypal integration

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import engines from 'consolidate';
+import paypal from 'paypal-rest-sdk';
 import colors from 'colors';
 
 import { Server } from 'socket.io';
@@ -14,6 +18,7 @@ import postRoutes from './routes/postRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import messageRoutes from './routes/messageRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
+import paypalRoutes from './routes/paypalRoutes.js';
 
 // Error handling middleware
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
@@ -24,6 +29,13 @@ connectDB();
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.engine('ejs', engines.ejs);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname + '/views/'));
+
 // Allows access to JSON data in the body
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ limit: '15MB' }));
@@ -33,19 +45,13 @@ app.use('/api/posts', postRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/upload', uploadRoutes);
+app.use('/api/paypal', paypalRoutes);
 
 app.get('/', (req, res) => {
-  res.send('API is running...');
+  res.send('API is running..');
 });
 
-// const __dirname = path.resolve();
-// app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-
-// Error handling middlewae
-app.use(notFound);
-app.use(errorHandler);
-
-const PORT = process.env.PORT || 5000;
+/* ----------------------------------------------------------------------------- */
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
@@ -55,6 +61,7 @@ const io = new Server(httpServer, {
   },
 });
 
+// SOCKET.IO
 const onSendMessage = (data) => {
   console.log(data);
   io.emit('receive_message', data);
@@ -77,6 +84,111 @@ const onConnection = (socket) => {
 };
 
 io.on('connection', onConnection);
+
+/* ----------------------------------------------------------------------------- */
+
+// PAYPAL
+paypal.configure({
+  mode: 'sandbox', // sandbox or live
+  client_id: process.env.PAYPAL_CLIENT_ID,
+  client_secret: process.env.PAYPAL_SECRET,
+});
+
+// // Renders index.ejs
+// app.get('/paypal', (req, res) => {
+//   res.render('index');
+// });
+
+// // CREATE ORDER
+// app.get('/paypal/createorder', (req, res) => {
+//   // const paymentId =
+
+//   var create_payment_json = {
+//     intent: 'sale',
+//     payer: {
+//       payment_method: 'paypal',
+//     },
+//     redirect_urls: {
+//       return_url: 'http://localhost:5000/api/paypal/success',
+//       cancel_url: 'http://localhost:5000/api/paypal/cancel',
+//     },
+//     transactions: [
+//       {
+//         item_list: {
+//           items: [
+//             {
+//               name: 'item',
+//               sku: 'item',
+//               price: '1.00',
+//               currency: 'USD',
+//               quantity: 1,
+//             },
+//           ],
+//         },
+//         amount: {
+//           currency: 'USD',
+//           total: '1.00',
+//         },
+//         description: 'This is the payment description.',
+//       },
+//     ],
+//   };
+
+//   paypal.payment.create(create_payment_json, (error, payment) => {
+//     if (error) {
+//       throw error;
+//     } else {
+//       console.log('Create Payment Response');
+//       console.log(payment);
+//       res.redirect(payment.links[1].href);
+//       // res.send('OK');
+//     }
+//   });
+// });
+
+// app.get('/paypal/success', (req, res) => {
+//   var PayerID = req.query.PayerID;
+//   var paymentId = req.query.paymentId;
+
+//   var execute_payment_json = {
+//     payer_id: PayerID,
+//     transactions: [
+//       {
+//         amount: {
+//           currency: 'USD',
+//           total: '1.00',
+//         },
+//       },
+//     ],
+//   };
+
+//   paypal.payment.execute(
+//     paymentId,
+//     execute_payment_json,
+//     function (error, payment) {
+//       if (error) {
+//         console.log(error.response);
+//         throw error;
+//       } else {
+//         console.log('Get Payment Response');
+//         console.log(JSON.stringify(payment));
+//         res.render('success');
+//       }
+//     }
+//   );
+// });
+
+// app.get('/paypal/cancel', (req, res) => {
+//   res.render('cancel');
+// });
+
+/* ----------------------------------------------------------------------------- */
+
+// Error handling middlewae
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
 
 httpServer.listen(
   PORT,
